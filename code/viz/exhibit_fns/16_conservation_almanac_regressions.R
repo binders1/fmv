8ba7a06caf2102bf99bfe76dc8dac6fb81c610e4
Asa Gold <gold1@stolaf.edu>
2023-03-10 17:23:09 -0600

    run conservation regressions for all three conservation measures, w/o FEs

---
+++ code/viz/exhibit_fns/16_conservation_almanac_regressions.R
@@ -0,0 +1,85 @@
+
+conservation_almanac_regressions <- function() {
+  
+}
+
+# Mark counties in ffb but left out of nolte_counties.csv
+
+## Read in predict_all for ffb, then mark county as "small"
+## if it does not appear in nolte_counties.csv
+
+nolte_counties <-
+  read_helper_data("nolte_counties.csv") %>%
+  pull(fips)
+
+ffb_counties <-
+  loadResults(
+    model = "ffb",
+    res_type = "predict_all"
+  ) %>%
+  mutate(
+    fips = str_sub(sid, 1, 5)
+    ) %>%
+  distinct(fips) %>%
+  mutate(
+    left_out_nolte = !(fips %in% nolte_counties),
+    left_out_nolte = as.numeric(left_out_nolte)
+      )
+
+# Read in conservation indicators
+conserve_by_mech <-
+  read_helper_data("conservation_almanac_counts.csv") %>%
+  dplyr::filter(!str_detect(fips, "~~")) %>%
+  split(.$protmech)
+
+# Merge with county shapefile
+conserve_counties_sf <-
+  reduce(
+    .x = list(us_counties, ffb_counties, conserve_by_mech$f),
+    .f = full_join,
+    by = "fips"
+  )
+
+
+# Normalize conservation indicator by county area
+normalize_conserve_measures <-
+  conserve_counties_sf %>%
+  mutate(
+    county_area_km2 = (ALAND + AWATER)/1e+06,
+    state = str_sub(fips, 1, 2),
+    across(
+      .cols = c(acreage, spending, count),
+      .fns = ~ .x / county_area_km2,
+      .names = "{.col}_per_km2"
+    )
+  ) %>%
+  na.omit()
+
+# Run regression
+# - outcome = conservation indicator
+# - treatment = whether county is small
+# - FEs: state
+
+outcome_vars <- 
+  c("acreage_per_km2",
+    "spending_per_km2",
+    "count_per_km2") %>%
+  purrr::set_names(nm = c("(1)", "(2)", "(3)"))
+
+conservation_regression <- function(outcome_var) {
+  
+  fml <- paste0(outcome_var, " ~ left_out_nolte")
+  
+  fixest::feols(
+    fml = as.formula(fml),
+    data = normalize_conserve_measures
+  ) 
+}
+
+map(
+  outcome_vars,
+  conservation_regression
+) %>%
+  fixest::etable()
+
+
